3430271c177e6c71864241879ca3d95c216b282c
agusL1660 <lelliag@hotmail.com>
2023-05-26 15:37:22 -0300

    actualizacion carrito

--- motomami/componentes/Motos.tsx
+++ motomami/componentes/Motos.tsx
@@ -3,31 +3,52 @@ import React, { useEffect, useState } from 'react';
 
 function Motos() {
   const [motos, setMotos] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [countProducts, setCountProducts] = useState(0);
+  const [allProducts, setAllProducts] = useState([]);
 
   useEffect(() => {
     fetch('https://circuit-crusaders-laravel-agusl1660.vercel.app/rest/motos')
       .then(response => response.json())
       .then(motos => setMotos(motos));
   }, []);
+
+  const addToCart = (product) => {
+    if (allProducts.find(item => item.id === product.id)) {
+      const products = allProducts.map(item =>
+        item.id === product.id
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      );
+      setTotal(total + product.price * product.quantity);
+      setCountProducts(countProducts + product.quantity);
+      return setAllProducts([...products]);
+    }
+
+    setTotal(total + product.price * product.quantity);
+    setCountProducts(countProducts + product.quantity);
+    setAllProducts([...allProducts, product]);
+  };
   
 
   return (
     <div>
-      <h2>Motos</h2>
-      <ul>
-        {motos.map(moto => (
-          <li key={moto.nro_moto}>
-            <h3>{moto.marca}</h3>
-            <p>Modelo: {moto.modelo}</p>
-            <p>Año: {moto.anio}</p>
-            <p>Cilindrada: {moto.cilindrada}</p>
-            <p>Patente: {moto.patente}</p>
-            <p>Estilo: {moto.id_estilo}</p>
-          </li>
+      <div className='container-items'>
+			  {motos.map(moto => (
+          <div className='item' key={moto.nro_moto}>
+              <div className='info-product'>
+                <h2>{moto.marca} {moto.modelo} {moto.id_estilo} {moto.anio} </h2>
+                <p className='price'>${moto.monto}</p>
+                <button onClick={() => addToCart(moto)}>
+							    Añadir al carrito
+						    </button>
+                </div>
+          </div>  
+
         ))}
-      </ul>
-    </div>
-  );
+      </div>
+      </div>
+    );
 }
 
 export default Motos;
