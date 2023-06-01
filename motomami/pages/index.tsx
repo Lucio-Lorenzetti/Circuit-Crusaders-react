11bfa123f08f2e7966d1e295667f23a0a2ea46ff
Lucio-Lorenzetti <89593565+Lucio-Lorenzetti@users.noreply.github.com>
2023-06-01 18:35:45 -0300

    Modificaciones varias sobre estilos e index

--- motomami/pages/index.tsx
+++ motomami/pages/index.tsx
@@ -1,7 +1,10 @@
 import Head from 'next/head';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import styles from '../styles/Home.module.css';
 import { NextPage } from 'next';
 import Link from 'next/link';
+import React from 'react';
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +13,34 @@ const Home: NextPage = () => {
         <title>MotoMami</title>
       </Head>
       <div className={styles['index-container']}>
-        {/*<div className={styles['background-image']}>*/}
+        <Carousel
+          className={styles['background-carousel']}
+          showStatus={false}
+          showThumbs={false}
+          infiniteLoop={true}
+          autoPlay={true}
+          interval={5000}
+          transitionTime={2000} 
+        >
+          <div>
+            <img src="https://www.galgo.com/wp-content/uploads/2023/04/motos-cafe-racer.jpeg" alt="Imagen 1" />
+          </div>
+          <div>
+            <img src="https://lamoto.com.ar/wp-content/uploads/2021/12/Motos-electricas-Ducati-prueba-lateral.jpg" alt="Imagen 2" />
+          </div>
+          <div>
+            <img src="https://fondosmil.com/fondo/43749.jpg" alt="Imagen 3" />
+          </div>
+          <div>
+            <img src="https://i.ytimg.com/vi/ma7K3DnPutM/maxresdefault.jpg" alt="Imagen 4" />
+          </div>
+          <div>
+            <img src="https://lamoto.com.ar/wp-content/uploads/2022/03/Moto-cafe-racer-no-es-para-cualquiera.jpg" alt="Imagen 5" />
+          </div>
+          <div>
+            <img src="https://images8.alphacoders.com/110/1105079.jpg" alt="Imagen 6" />
+          </div>
+        </Carousel>
         <div className={styles['content-container']}>
           <div className={styles['logo-container']}>
             <img src="https://i.ibb.co/W2Pmn2H/logomotomami.png" alt="Logo de la concesionaria" className={styles['logo']} />
@@ -21,8 +51,6 @@ const Home: NextPage = () => {
             </Link>
           </div>
         </div>
-        {/*</div>*/}
-        
       </div>
     </>
   );
