04aad04f933f3e015eb32e1d815c9c978d6f6868
Lucio-Lorenzetti <89593565+Lucio-Lorenzetti@users.noreply.github.com>
2023-06-01 21:24:53 -0300

    Modificaciones varias

--- motomami/pages/carrito/compras.tsx
+++ motomami/pages/carrito/compras.tsx
@@ -4,92 +4,93 @@ import { Motos } from '../../componentes/Motos';
 import { Confirmar } from '../../componentes/Confirmar';
 import { FiltroEstilos } from '../../componentes/FiltroEstilos';
 
-
 function Compras() {
-	const [allProducts, setAllProducts] = useState([]);
-	const [motos, setMotos] = useState([]);
-	const [total, setTotal] = useState(0);
-	const [countProducts, setCountProducts] = useState(0);
-	const [isDivVisible, setDivVisible] = useState(true);
-	const [id_estilo, setEstiloActual] = useState("");
+  const [allProducts, setAllProducts] = useState([]);
+  const [motos, setMotos] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [countProducts, setCountProducts] = useState(0);
+  const [isDivVisible, setDivVisible] = useState(true);
+  const [id_estilo, setEstiloActual] = useState("");
 
+  useEffect(() => {
+    if (id_estilo === "") {
+      fetch('https://circuit-crusaders-laravel-agusl1660.vercel.app/rest/motos')
+        .then((response) => response.json())
+        .then((motos) => setMotos(motos));
+    } else {
+      const obtenerMotosPorEstilo = async () => {
+        try {
+          fetch(`https://circuit-crusaders-laravel-agusl1660.vercel.app/rest/motos/estilos/${id_estilo}`)
+            .then((response) => response.json())
+            .then((motos) => setMotos(motos));
+        } catch (error) {
+          console.error(error);
+        }
+      };
 
-	useEffect(() => {
-		if (id_estilo === "") {
-		  	fetch('https://circuit-crusaders-laravel-agusl1660.vercel.app/rest/motos')
-			.then((response) => response.json())
-			.then((motos) => setMotos(motos));
-		} else {
-		  const obtenerMotosPorEstilo = async () => {
-			try {
-				fetch(`https://circuit-crusaders-laravel-agusl1660.vercel.app/rest/motos/estilos/${id_estilo}`)
-				.then((response) => response.json())
-				.then((motos) => setMotos(motos));
-				
-			} catch (error) {
-			  console.error(error);
-			}
-		  };
-	  
-		  obtenerMotosPorEstilo();
-		}
-	  }, [id_estilo]);
-	  
-  
-	return (
-		<>
-			<FiltroEstilos
-				allProducts={allProducts}
-				setAllProducts={setAllProducts}
-				total={total}
-				setTotal={setTotal}
-				countProducts={countProducts}
-				setCountProducts={setCountProducts}
-				isDivVisible={isDivVisible}
-				setDivVisible={setDivVisible}
-				id_estilo={id_estilo}
-				setEstiloActual={setEstiloActual}
-				motos={motos}
-				setMotos={setMotos}
-			 /> 
-			
+      obtenerMotosPorEstilo();
+    }
+  }, [id_estilo]);
 
-			<Header
-				allProducts={allProducts}
-				setAllProducts={setAllProducts}
-				total={total}
-				setTotal={setTotal}
-				countProducts={countProducts}
-				setCountProducts={setCountProducts}
-				isDivVisible={isDivVisible}
-				setDivVisible={setDivVisible}
-			/>
-			<Motos
-				allProducts={allProducts}
-				setAllProducts={setAllProducts}
-				total={total}
-				setTotal={setTotal}
-				countProducts={countProducts}
-				setCountProducts={setCountProducts}
-				isDivVisible={isDivVisible}
-				setDivVisible={setDivVisible}
-				id_estilo={id_estilo}
-				setEstiloActual={setEstiloActual}
-				motos={motos}
-				setMotos={setMotos}
-			/>
-            <Confirmar
-				allProducts={allProducts}
-				setAllProducts={setAllProducts}
-				total={total}
-				setTotal={setTotal}
-				countProducts={countProducts}
-				setCountProducts={setCountProducts}
-				isDivVisible={isDivVisible}
-				setDivVisible={setDivVisible}
-			/>
-		</>
-	);
+  return (
+    <>
+      <div style={{ display: "flex" }}>
+        <div style={{ width: "85%" }}>
+          <Header
+            allProducts={allProducts}
+            setAllProducts={setAllProducts}
+            total={total}
+            setTotal={setTotal}
+            countProducts={countProducts}
+            setCountProducts={setCountProducts}
+            isDivVisible={isDivVisible}
+            setDivVisible={setDivVisible}
+          />
+          <Motos
+            allProducts={allProducts}
+            setAllProducts={setAllProducts}
+            total={total}
+            setTotal={setTotal}
+            countProducts={countProducts}
+            setCountProducts={setCountProducts}
+            isDivVisible={isDivVisible}
+            setDivVisible={setDivVisible}
+            id_estilo={id_estilo}
+            setEstiloActual={setEstiloActual}
+            motos={motos}
+            setMotos={setMotos}
+          />
+          <Confirmar
+            allProducts={allProducts}
+            setAllProducts={setAllProducts}
+            total={total}
+            setTotal={setTotal}
+            countProducts={countProducts}
+            setCountProducts={setCountProducts}
+            isDivVisible={isDivVisible}
+            setDivVisible={setDivVisible}
+          />
+        </div>
+		
+		</div>
+        <div style={{ width: "40%" }}>
+          <FiltroEstilos
+            allProducts={allProducts}
+            setAllProducts={setAllProducts}
+            total={total}
+            setTotal={setTotal}
+            countProducts={countProducts}
+            setCountProducts={setCountProducts}
+            isDivVisible={isDivVisible}
+            setDivVisible={setDivVisible}
+            id_estilo={id_estilo}
+            setEstiloActual={setEstiloActual}
+            motos={motos}
+            setMotos={setMotos}
+          />
+        </div>
+    </>
+  );
 }
 
 export default Compras;
